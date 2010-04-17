69558b8ec6e22987b61121a4ce59a9c6c2027ab2
Karl Varga <kjvarga@gmail.com>
2010-04-16 18:06:43 -0700

    Update README. Add install code. Move the decision tree into the Ajax plugin.

--- install.rb
+++ install.rb
@@ -1,1 +1,17 @@
-# Install hook code here
+AJAX_ROOT = File.dirname(__FILE__)
+
+%w[
+  config/initializers/ajax.rb 
+  public/javascripts/ajax.js
+  public/javascripts/jquery.address-1.2rc.js
+  public/javascripts/jquery.address-1.2rc.min.js
+  public/javascripts/jquery.json-2.2.min.js
+  public/images/loading-icon-small.gif
+].each do |file|
+  if File.exist?(File.join(RAILS_ROOT, file))
+    puts "already exists: #{file}, file not copied"
+  else
+    FileUtils.cp(File.join(AJAX_ROOT, file), File.join(RAILS_ROOT, file))
+    puts "created: #{file}"
+  end
+end
--- lib/rack-ajax.rb
+++ lib/rack-ajax.rb
@@ -3,7 +3,7 @@ require 'json'
 
 module Rack
   class Ajax
-    cattr_accessor :decision_tree
+    cattr_accessor :decision_tree, :default_decision_tree
     attr_accessor :user, :request, :params
 
     # If called with a block, executes that block as the "decision tree".
@@ -14,7 +14,7 @@ module Rack
     # decision tree will be used unless a block is provided.
     def initialize(app)
       @app = app
-      @decision_tree = block_given? ? Proc.new : self.class.decision_tree
+      @decision_tree = block_given? ? Proc.new : (self.class.decision_tree || self.class.default_decision_tree)
     end
 
     def call(env)
@@ -57,5 +57,47 @@ module Rack
       # @see http://blog.codefront.net/2009/06/15/activerecord-rails-metal-too-many-connections/
       ::ActiveRecord::Base.clear_active_connections! if defined?(::ActiveRecord::Base)
     end
+    
+    def default_decision_tree
+      @@default_decision_tree ||= Proc.new do
+        ::Ajax.logger.debug("[ajax] rack session #{@env['rack.session'].inspect}")
+        ::Ajax.logger.debug("[ajax] Ajax-Info #{@env['Ajax-Info'].inspect}")
+
+        if !::Ajax.exclude_path?(@env['PATH_INFO'] || @env['REQUEST_URI'])
+          if ajax_request?
+            if hashed_url? # the browser never sends the hashed part
+              rewrite_to_traditional_url_from_fragment
+            end
+          else
+            if url_is_root?
+              if hashed_url? # the browser never sends the hashed part
+                rewrite_to_traditional_url_from_fragment
+              elsif get_request? && !user_is_robot?
+                # When we render the framework we would like to show the
+                # page the user wants on the first request.  If the
+                # session has a value for <tt>redirected_to</tt> then
+                # that page will be rendered.
+                if redirected_to = (@env['rack.session'][:redirected_to] || @env['rack.session']['redirected_to'])
+                  redirected_to = ::Ajax.is_hashed_url?(redirected_to) ? ::Ajax.traditional_url_from_fragment(redirected_to) : redirected_to
+                  ::Ajax.logger.debug("[ajax] showing #{redirected_to} instead of root_url")
+                  rewrite(redirected_to)
+                else
+                  rewrite_to_render_ajax_framework
+                end
+              end
+            else
+              if !user_is_robot?
+                if hashed_url? # will never be true
+                  redirect_to_hashed_url_from_fragment
+                else
+                  if get_request?
+                    redirect_to_hashed_url_equivalent
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
   end
 end
